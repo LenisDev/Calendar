4ec1673ffc5f93b9ce8d207c0f875dea45150370
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-25 11:22:56 +0530

    Added framework

---
+++ Calendar/Calendar/Calendar.h
@@ -0,0 +1,19 @@
+//
+//  Calendar.h
+//  Calendar
+//
+//  Created by Manish on 25/08/20.
+//  Copyright © 2020 Manish. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calendar.
+FOUNDATION_EXPORT double CalendarVersionNumber;
+
+//! Project version string for Calendar.
+FOUNDATION_EXPORT const unsigned char CalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calendar/PublicHeader.h>
+
+
